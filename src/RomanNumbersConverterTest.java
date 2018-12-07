0955d8d8f98be24c35826abf20e99eb9120fb04c
creat73 <nikill1125@gmail.com>
2018-12-07 14:16:41 +0100

    Add more border value tests

--- src/RomanNumbersConverterTest.java
+++ src/RomanNumbersConverterTest.java
@@ -15,6 +15,36 @@ public class RomanNumbersConverterTest {
     }
 
     @Test
+    public void converting4ShouldReturnIV(){
+        Assert.assertEquals("IV",converter.Convert(4));
+    }
+
+    @Test
+    public void converting9ShouldReturnIX(){
+        Assert.assertEquals("IX",converter.Convert(9));
+    }
+
+    @Test
+    public void converting49ShouldReturnXLIX(){
+        Assert.assertEquals("XLIX",converter.Convert(49));
+    }
+
+    @Test
+    public void converting99ShouldReturnXCIX(){
+        Assert.assertEquals("XCIX",converter.Convert(99));
+    }
+
+    @Test
+    public void converting499ShouldReturnCDXCIX(){
+        Assert.assertEquals("CDXCIX",converter.Convert(499));
+    }
+
+    @Test
+    public void converting999ShouldReturnCMXCIX(){
+        Assert.assertEquals("CMXCIX",converter.Convert(999));
+    }
+
+    @Test
     public void convertingHighestNumberShouldReturnMMMMCMXCIX(){
         Assert.assertEquals("MMMMCMXCIX", converter.Convert(4999));
     }
